3ce6dcbdfe71c181b8c1ff2a1b5ed707ee5999ca
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2026-06-15 19:49:31 +0300

    where related query

--- QUERY.sql
+++ QUERY.sql
@@ -107,3 +107,27 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+
+-- Part 2
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+select match_id, fixture, base_ticket_price from Matches 
+where tournament_category = 'Champions League' and match_status = 'Available';
+
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+select user_id, full_name, email from Users
+where full_name ilike 'Tanvir%' or full_name ilike '%Haque%';
+
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+select booking_id, user_id, match_id, coalesce(payment_status, 'Action Required') as systematic_status from Bookings
+where payment_status is null;
+
+
