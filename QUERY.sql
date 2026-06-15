812221e48e41e934620523692d54d71f7ddbf2fd
Pamel-C223171 <Mohammadpamel12345@gmail.com>
2026-06-15 19:43:47 +0300

    create to the users table

--- QUERY.sql
+++ QUERY.sql
@@ -12,3 +12,22 @@ create database football_ticket_booking_system
 DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
+
+
+-- =========================================================================
+-- 1. CREATE USERS TABLE
+-- =========================================================================
+CREATE TABLE Users (
+    user_id serial,
+    full_name varchar(100) not null,
+    email varchar(100) not null,
+    role varchar(20) not null,
+    phone_number varchar(15),
+    
+    -- Write your constraint to make 'user_id' the Primary Key
+  primary key (user_id),
+    -- Write your constraint to ensure 'email' values are never duplicated
+  unique (email),
+    -- Write your check constraint to restrict 'role' to specific allowed strings
+  check(role in ('Ticket Manager', 'Football Fan'))
+);
